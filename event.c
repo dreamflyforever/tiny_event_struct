f6faf8f3eeee75725fa5dacec97029e115a12fc4
Shanjin Yang <shanjin.yang@aispeech.com>
2017-05-24 10:15:24 +0800

    add judge of entrance function
    Signed-off-by: Shanjin Yang <shanjin.yang@aispeech.com>

--- event.c
+++ event.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 
-#define EVENT_NUMBER 10
-typedef int (*cb)(void *arg);
-cb func[EVENT_NUMBER];
+/*system support event number*/
+#define EVENT_NUMBER 2
+
+/*the callback function of event*/
+typedef int (*CB)(void *arg);
+CB func[EVENT_NUMBER];
 
 int event_init()
 {
@@ -13,11 +16,16 @@ int event_init()
 	return 0;
 }
 
-int reg(cb user_func, int event)
+int reg(CB user_func, int event)
 {
-	if ((user_func == NULL) && (event >= EVENT_NUMBER))
+	if ((user_func == NULL) && (event >= EVENT_NUMBER)) {
+		printf("error: %s event(%d) >= EVENT_NUMBER(%d)\n",
+			__func__, event, EVENT_NUMBER);
 		return -1;
-
+	}
+	if (func[event] != NULL) {
+		printf("attention: event is registered, now overwrite\n");
+	}
 	func[event] = user_func;
 
 	return 0;
@@ -25,8 +33,9 @@ int reg(cb user_func, int event)
 
 int unreg(int event)
 {
-	if (event > EVENT_NUMBER) {
-		printf("event > EVENT_NUMBER\n");
+	if (event >= EVENT_NUMBER) {
+		printf("error: %s event(%d) >= EVENT_NUMBER(%d)\n",
+			__func__, event, EVENT_NUMBER);
 		return -1;
 	}
 
@@ -36,20 +45,23 @@ int unreg(int event)
 
 int send(int event, void *arg)
 {
-	if (event > EVENT_NUMBER) {
-		printf("event > EVENT_NUMBER\n");
+	if (event >= EVENT_NUMBER) {
+		printf("error %s event(%d) >= EVENT_NUMBER(%d)\n",
+			__func__, event, EVENT_NUMBER);
 		return -1;
 	}
 
 	if (func[event] != NULL)
 		func[event](arg);
-
+	else
+		printf("event(%d) not register\n", event);
 	return 0;
 }
 
 int hello(void *arg)
 {
-	printf("%s\n", (char *)arg);
+	if (arg != NULL)
+		printf("%s\n", (char *)arg);
 
 	return 0;
 }
@@ -58,8 +70,15 @@ int main()
 {
 	event_init();
 	reg(hello, 1);
+	reg(hello, 1);
 	reg(hello, 2);
+	reg(hello, 0);
+	reg(hello, 0);
 	send(1, "abc");
 	send(2, "defg");
+	send(2, "defg");
+	send(2, "defg");
+	send(6, "defg");
+	send(0, "defg");
 	return 0;
 }
